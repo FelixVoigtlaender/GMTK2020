ec584e03cfde59160c43a2d63d14d647376a91fa
wentzler <felixwentzel@gmail.com>
2020-07-11 13:12:52 +0200

    Refactored: Tilemanager added

--- GMTK2020/Assets/Scripts/FireSystem.cs
+++ GMTK2020/Assets/Scripts/FireSystem.cs
@@ -1,57 +1,28 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class FireSystem : MonoBehaviour
 {
-	[SerializeField] Image imageForMap = default;
 
-	[SerializeField] int mapSideLength = 50;
+	
 	[SerializeField] Tile[] fireSeedpoints;
-	[SerializeField] Gradient gradient;
 	[SerializeField] int fireGrowthSpeed = 10;
-
-	Tile[,] tiles;
-	Texture2D map;
-
-
-	private void Awake()
-	{
-		map = new Texture2D(mapSideLength, mapSideLength);
-		map.filterMode = FilterMode.Point;
-
-		imageForMap.material.mainTexture = map;
-		tiles = new Tile[mapSideLength, mapSideLength];
-
-		//Fill in Seedpoints
-		foreach (Tile fireSeed in fireSeedpoints)
-		{
-			tiles[fireSeed.x, fireSeed.y] = fireSeed;
-			tiles[fireSeed.x, fireSeed.y].changeFireValue(fireGrowthSpeed);
-		}
-
-		//Fill rest of the map excluding already set seedpoints
-		for (int x = 0; x < mapSideLength; x++)
-		{
-			for (int y = 0; y < mapSideLength; y++)
-			{
-				if (tiles[x, y] == null)
-					tiles[x, y] = new Tile(0, x, y);
-			}
-		}
-	}
+	
 
 	private void Start()
 	{
+		//Fill in Seedpoints
+		foreach (Tile fireSeed in fireSeedpoints)
+		{
+			TileManager.singleton.tiles[fireSeed.x, fireSeed.y] = fireSeed;
+			TileManager.singleton.tiles[fireSeed.x, fireSeed.y].changeFireValue(fireGrowthSpeed);
+		}
 		GameManager.singleton.onNewTick += UpdateFire;
 	}
 
 	void UpdateFire()
 	{
 		CalculateNewFirevalues();
-		UpdateImage();
+		TileManager.singleton.UpdateImage();
 		print("updated Fires");
 	}
 
@@ -61,12 +32,13 @@ public class FireSystem : MonoBehaviour
 		//Cache current winddirection
 		int windDirX = WindSystem.singleton.xDir;
 		int windDirY = WindSystem.singleton.yDir;
+		int mapSideLength = TileManager.singleton.mapSideLength;
 
 		for (int x = 0; x < mapSideLength; x++)
 		{
 			for (int y = 0; y < mapSideLength; y++)
 			{
-				Tile tile = tiles[x, y];
+				Tile tile = TileManager.singleton.tiles[x, y];
 				if (tile.fireValue > 0)
 				{
 					//Dont calculate wind, if its a new fire (fire that started this gameTick)
@@ -78,21 +50,18 @@ public class FireSystem : MonoBehaviour
 							windY > 0 && windY < mapSideLength)
 						{
 							//Increment firevalue in cell affected by wind
-							tiles[windX, windY].changeFireValue(1);
+							TileManager.singleton.tiles[windX, windY].changeFireValue(1);
 						}
 						if (x > 0 && x < mapSideLength-1 &&
 							y > 0 && y < mapSideLength-1)
 						{
 							//start fires in adjacent cells
-						tiles[x + 1, y + 1].changeFireValue(1);
-						tiles[x + 1, y - 1].changeFireValue(1);
-						tiles[x - 1, y + 1].changeFireValue(1);
-						tiles[x - 1, y - 1].changeFireValue(1);
+							TileManager.singleton.tiles[x + 1, y + 1].changeFireValue(1);
+							TileManager.singleton.tiles[x + 1, y - 1].changeFireValue(1);
+							TileManager.singleton.tiles[x - 1, y + 1].changeFireValue(1);
+							TileManager.singleton.tiles[x - 1, y - 1].changeFireValue(1);
 						}
 					}
-
-					
-
 					//Increment firevalue in this cell
 					tile.changeFireValue(fireGrowthSpeed);
 				}
@@ -100,18 +69,5 @@ public class FireSystem : MonoBehaviour
 		}
 	}
 
-	private void UpdateImage()
-	{
-		for (int x = 0; x < map.width; x++)
-		{
-			for (int y = 0; y < map.height; y++)
-			{
-				//Evaluate Gradient based on firevalue of the Tile
-				Color color = gradient.Evaluate(Mathf.Lerp(0,1,Mathf.InverseLerp(-3,255, tiles[x, y].fireValue)));
-				map.SetPixel(x, y, color);
-			}
-		}
-		//Applies changed colors to Image
-		map.Apply();
-	}
+	
 }
